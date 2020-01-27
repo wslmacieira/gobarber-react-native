6129816154b5faea19d9e8cf1110dfe116fa4832
wslmacieira <wslmacieira@DESKTOP-GJDMPV4.localdomain>
2020-01-27 13:17:45 -0300

    Reactotron

---
+++ src/config/ReactotronConfig.js
@@ -0,0 +1,17 @@
+import Reactotron from 'reactotron-react-native';
+import { reactotronRedux } from 'reactotron-redux';
+import reactotroSaga from 'reactotron-redux-saga';
+import AsyncStorage from '@react-native-community/async-storage';
+
+if (__DEV__) {
+  const tron = Reactotron.setAsyncStorageHandler(AsyncStorage)
+    .configure({ host: '192.168.0.105' })
+    .useReactNative()
+    .use(reactotronRedux())
+    .use(reactotroSaga())
+    .connect();
+
+  tron.clear();
+
+  console.tron = tron;
+}
